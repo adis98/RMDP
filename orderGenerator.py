b40c8559f7ce2aa599a9a62a2124f20981d529ea
adis98 <39487375+adis98@users.noreply.github.com>
2022-05-03 17:28:11 +0200

    Add files via upload

---
+++ orderGenerator.py
@@ -0,0 +1,56 @@
+import numpy as np
+import random
+import time
+import pickle
+
+mapSize = 100 #creates an N by N 2D grid
+simulationDuration = 400 #number of seconds to run the simulation for
+vehicleCrossTime = 20 #each vehicle takes crosstime steps to go from top to bottom or from left to right across the grid
+numInitialOrders = 60 #number of orders available at timestep 0
+deliveryWindowTime = 60
+minOrderReadyTime = 5 #min time it takes for an order to be prepared
+maxOrderReadyTime = 15
+maxOrderArrivalTime = 200 #upper bound on time after which new orders will not be accepted
+arrivalRate = 0.02 #the probability of an order arriving at any given time step
+
+
+class Order():
+    def __init__(self):
+        self.customerX = 0
+        self.customerY = 0
+        self.restaurantX = 0
+        self.restaurantY = 0
+        self.maxDeliveryTime = 0
+        self.minDeliveryTime = 0
+        self.orderPlacedTime = 0
+
+    def createOrder(mapSize,currentTime,minReadyTime,maxReadyTime,deliveryWindowTime): #chooses the customer location and the restaurant number
+            order = Order()
+            order.customerX,order.customerY,order.restaurantX,order.restaurantY = np.random.randint(mapSize,size=4)
+            order.maxDeliveryTime = currentTime+deliveryWindowTime
+            order.minDeliveryTime = random.randint(minReadyTime,maxReadyTime+1) + currentTime
+            order.orderPlacedTime = currentTime
+            return order
+
+def generateOrders():
+    orders = []
+    for i in range(numInitialOrders):
+        orders.append(Order.createOrder(mapSize,0,minOrderReadyTime,maxOrderReadyTime,deliveryWindowTime))
+    for currTime in range(maxOrderArrivalTime):
+        numOrders = np.random.poisson(arrivalRate)
+        for j in range(numOrders):
+            orders.append(Order.createOrder(mapSize,currTime,minOrderReadyTime,maxOrderReadyTime,deliveryWindowTime))
+    return orders
+
+        #print(numOrders)
+
+            #order = createOrder(mapSize,i+deliveryWindowTime)
+if __name__=="__main__":
+    fileName = "orders.pkl"
+    orders = generateOrders() #returns a list of orders for the problem
+    with open(fileName, "wb") as f:
+        pickle.dump(orders, f)
+    with open(fileName, "rb") as f:
+        orders = pickle.load(f)
+        for order in orders:
+            print(order.customerX,order.customerY,order.minDeliveryTime,order.maxDeliveryTime)
